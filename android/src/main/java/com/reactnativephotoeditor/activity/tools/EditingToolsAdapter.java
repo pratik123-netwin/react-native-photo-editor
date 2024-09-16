a50a5473d7cc5c15355ba10d4af4e43c4eb800df
krunal karanjekar <krunal.karanjekar@netinfo.com>
2024-09-16 11:00:26 +0530

    commit with chnagin the tool name

--- android/src/main/java/com/reactnativephotoeditor/activity/tools/EditingToolsAdapter.java
+++ android/src/main/java/com/reactnativephotoeditor/activity/tools/EditingToolsAdapter.java
@@ -26,10 +26,10 @@ public class EditingToolsAdapter extends RecyclerView.Adapter<EditingToolsAdapte
 
   public EditingToolsAdapter(OnItemSelected onItemSelected) {
     mOnItemSelected = onItemSelected;
-    mToolList.add(new ToolModel("Shape", R.drawable.ic_brush, ToolType.SHAPE));
+    mToolList.add(new ToolModel("Draw", R.drawable.ic_brush, ToolType.SHAPE));
     mToolList.add(new ToolModel("Eraser", R.drawable.ic_eraser, ToolType.ERASER));
   //  mToolList.add(new ToolModel("Filter", R.drawable.ic_colorfilter, ToolType.FILTER));
-    mToolList.add(new ToolModel("Arrow/Cross", R.drawable.ic_sticker, ToolType.STICKER));
+    mToolList.add(new ToolModel("Shapes", R.drawable.ic_sticker, ToolType.STICKER));
     mToolList.add(new ToolModel("Text", R.drawable.ic_smallcaps, ToolType.TEXT));
   }
 
