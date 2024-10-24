8a5e2bbca02cda107ed98b57dd5b2147f6294324
pratik.waghsakar <pratik.wahhaskar@netwininfo.com>
2024-10-24 12:06:12 +0530

    commit with adding 50 min side

--- ios/ZLImageEditor/Sources/General/ZLImageStickerView.swift
+++ ios/ZLImageEditor/Sources/General/ZLImageStickerView.swift
@@ -368,7 +368,7 @@ class ZLImageStickerView: UIView, ZLStickerViewAdditional {
     
     class func calculateSize(image: UIImage, width: CGFloat) -> CGSize {
         let maxSide = width / 2
-        let minSide: min(image.size.width, image.size.height)
+        let minSide: 50
         let whRatio = image.size.width / image.size.height
         var size: CGSize = .zero
         if whRatio >= 1 {
